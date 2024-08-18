d2c4db5489bbc474b83c5d82d1c71c85ee63e1f3
Taimoor Fazal <taimoorf@uio.no>
2024-08-18 15:53:12 +0200

    feat(chat-view): Set up basic chat view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,31 @@
 import React from "react";
 import CSS from "./pages.module.css";
 export default function Home() {
-  return <div className={CSS.container}></div>;
+  return (
+    <div className={CSS.container}>
+      <div className={CSS.body}>
+        <span className={CSS.list}>
+          <span className={CSS.chatTile}>Chat 1</span>
+          <span className={CSS.chatTile}>Chat 2</span>
+          <span className={CSS.chatTile}>Chat 3</span>
+          <span className={CSS.chatTile}>Chat 4</span>
+          <span className={CSS.chatTile}>Chat 5</span>
+          <span className={CSS.chatTile}>Chat 6</span>
+          <span className={CSS.chatTile}>Chat 7</span>
+          <span className={CSS.chatTile}>Chat 1</span>
+          <span className={CSS.chatTile}>Chat 2</span>
+          <span className={CSS.chatTile}>Chat 3</span>
+          <span className={CSS.chatTile}>Chat 4</span>
+          <span className={CSS.chatTile}>Chat 5</span>
+          <span className={CSS.chatTile}>Chat 6</span>
+          <span className={CSS.chatTile}>Chat 6</span>
+          <span className={CSS.chatTile}>Chat 4</span>
+          <span className={CSS.chatTile}>Chat 5</span>
+          <span className={CSS.chatTile}>Chat 6</span>
+          <span className={CSS.chatTile}>Chat 6</span>
+        </span>
+        <span className={CSS.chatBody}>Expanded chats</span>
+      </div>
+    </div>
+  );
 }
