4526c567cb0c1f32288dd6951e95fa3f8032acc5
Taimoor Fazal <taimoorf@uio.no>
2024-08-18 16:59:03 +0200

    feat(chat): Implement search friend feature and add textfields

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import CSS from "./pages.module.css";
+import { friends } from "../data/sample-data";
+import { User } from "../types/global-types";
 export default function Home() {
+  const [filteredFriends, setFilteredFriends] = useState<User[]>(friends);
   return (
     <div className={CSS.container}>
       <div className={CSS.body}>
         <span className={CSS.list}>
-          <span className={CSS.chatTile}>Chat 1</span>
-          <span className={CSS.chatTile}>Chat 2</span>
-          <span className={CSS.chatTile}>Chat 3</span>
-          <span className={CSS.chatTile}>Chat 4</span>
-          <span className={CSS.chatTile}>Chat 5</span>
-          <span className={CSS.chatTile}>Chat 6</span>
-          <span className={CSS.chatTile}>Chat 7</span>
-          <span className={CSS.chatTile}>Chat 1</span>
-          <span className={CSS.chatTile}>Chat 2</span>
-          <span className={CSS.chatTile}>Chat 3</span>
-          <span className={CSS.chatTile}>Chat 4</span>
-          <span className={CSS.chatTile}>Chat 5</span>
-          <span className={CSS.chatTile}>Chat 6</span>
-          <span className={CSS.chatTile}>Chat 6</span>
-          <span className={CSS.chatTile}>Chat 4</span>
-          <span className={CSS.chatTile}>Chat 5</span>
-          <span className={CSS.chatTile}>Chat 6</span>
-          <span className={CSS.chatTile}>Chat 6</span>
+          <span className={CSS.searchFriendField}>
+            <input
+              className={CSS.searchTextbox}
+              placeholder="Search friends"
+              type="text"
+              onChange={(e) => {
+                const searchValue = e.target.value;
+                setFilteredFriends(
+                  friends.filter((friend) =>
+                    friend.name
+                      .toLowerCase()
+                      .includes(searchValue.toLowerCase())
+                  )
+                );
+              }}
+            />
+          </span>
+          {filteredFriends.length === 0 && <i>No friends found</i>}
+
+          {filteredFriends.map((friend) => (
+            <FriendInfo key={friend.id} friend={friend} />
+          ))}
         </span>
-        <span className={CSS.chatBody}>Expanded chats</span>
+        <span className={CSS.chatBody}>
+          <textarea
+            className={CSS.chatBoxTextArea}
+            placeholder="Type a message"
+          />
+        </span>
       </div>
     </div>
   );
 }
+
+const FriendInfo = ({ friend }: { friend: User }) => {
+  return (
+    <span key={friend.id} className={CSS.chatTile}>
+      {friend.name}
+    </span>
+  );
+};
